e5ab81ecd9f5793c6d287988e42efe10bbb5f745
OddEer0 <OddEer0@gmail.com>
2023-06-21 12:32:08 +0600

    [task-3/feature]: add layout logic

--- src/widgets/MainLayout/ui/Aside/index.tsx
+++ src/widgets/MainLayout/ui/Aside/index.tsx
@@ -1,11 +1,25 @@
-import { FC } from "react"
+import { HTMLMotionProps } from "framer-motion"
+import { motion } from "framer-motion"
+import { FC, useContext } from "react"
+
+import { LayoutContext } from "@/widgets/MainLayout/ui/Layout/layout.provider.tsx"
 
 import styles from "./styles.module.scss"
 
-export const MainAside: FC = () => {
+type MainAsideProps = HTMLMotionProps<"div">
+
+export const MainAside: FC<MainAsideProps> = (...props) => {
+	const { isOpen, openHandler, closeHandler } = useContext(LayoutContext)
+
+	const toggleHandle = () => {
+		isOpen ? closeHandler() : openHandler()
+	}
+
 	return (
-		<div className={styles.aside}>
-			<div className=""></div>
-		</div>
+		<motion.aside className={styles.aside} {...props}>
+			<div className="" onClick={toggleHandle}>
+				B
+			</div>
+		</motion.aside>
 	)
 }
--- src/widgets/MainLayout/ui/Layout/index.tsx
+++ src/widgets/MainLayout/ui/Layout/index.tsx
@@ -1,12 +1,7 @@
 import { animate, motion, useMotionValue } from "framer-motion"
 import { FC, PropsWithChildren } from "react"
 
-import {
-	ASIDE_CLOSE_WIDTH,
-	ASIDE_OPEN_WIDTH,
-	MAIN_CLOSE_X,
-	MAIN_OPEN_X,
-} from "@/widgets/MainLayout/lib.ts"
+import { ASIDE_CLOSE_WIDTH, ASIDE_OPEN_WIDTH } from "@/widgets/MainLayout/lib.ts"
 
 import { Container } from "@/shared/ui"
 
@@ -16,17 +11,14 @@ import { LayoutProvider } from "./layout.provider.tsx"
 import styles from "./styles.module.scss"
 
 export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
-	const motionXValue = useMotionValue(0)
 	const motionWidthValue = useMotionValue(ASIDE_CLOSE_WIDTH)
 
 	const openHandle = () => {
 		animate(motionWidthValue, ASIDE_OPEN_WIDTH)
-		animate(motionXValue, MAIN_OPEN_X)
 	}
 
 	const closeHandle = () => {
 		animate(motionWidthValue, ASIDE_CLOSE_WIDTH)
-		animate(motionXValue, MAIN_CLOSE_X)
 	}
 
 	return (
@@ -35,7 +27,7 @@ export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
 				<motion.aside className={styles.aside} style={{ width: motionWidthValue }}>
 					<MainAside />
 				</motion.aside>
-				<motion.main style={{ x: motionXValue }}>{children}</motion.main>
+				<motion.main>{children}</motion.main>
 			</Container>
 		</LayoutProvider>
 	)
