a8b3e11db9ea2e92ef883cae53e223969887dee1
OddEer0 <OddEer0@gmail.com>
2023-06-26 17:52:56 +0600

    [task-9/feature]: modify Column

--- src/entities/Column/ui/TaskColumn/index.tsx
+++ src/entities/Column/ui/TaskColumn/index.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from "react"
+import { FC, HTMLAttributes, ReactNode } from "react"
 
 import { Column } from "@/shared/api"
 import { classname } from "@/shared/package/classname"
@@ -8,18 +8,27 @@ import styles from "./styles.module.scss"
 
 interface TaskColumnProps extends HTMLAttributes<HTMLDivElement> {
 	column: Column
+	edit?: ReactNode
+	addTask?: ReactNode
 }
 
-export const TaskColumn: FC<TaskColumnProps> = ({ column, className, ...props }) => {
+export const TaskColumn: FC<TaskColumnProps> = ({
+	column,
+	edit: Edit,
+	addTask: AddTask,
+	className,
+	...props
+}) => {
 	const classes = classname(styles.column, className)
 	const { bg, color, name } = column
 
 	return (
-		<Box sx={{ background: bg, color }} className={classes} {...props}>
-			<div className={styles.head}>
+		<Box className={classes} {...props}>
+			<div className={styles.head} style={{ background: bg, color }}>
 				<Text>{name}</Text>
+				{Edit && Edit}
 			</div>
-			<div className={styles.body}></div>
+			<div className={styles.body}>{AddTask && AddTask}</div>
 		</Box>
 	)
 }
