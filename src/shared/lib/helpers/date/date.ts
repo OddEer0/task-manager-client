d5ebcb45de9b3748b1133d7eae1fc1694c110e26
OddEer0 <OddEer0@gmail.com>
2023-07-07 18:20:00 +0300

    fix helper

--- src/shared/lib/helpers/date/date.ts
+++ src/shared/lib/helpers/date/date.ts
@@ -2,14 +2,16 @@ import { firstLetterUpperCase } from "@/shared/lib"
 
 export const getMonthDateAndWeekDay = (data: string, locale = "ru") => {
 	const date = new Date(data)
-	const weekday = date.toLocaleString(locale, { weekday: "short", day: "numeric", month: "short" })
+	const weekday = date.toLocaleString(locale, {
+		weekday: "short",
+		day: "numeric",
+		month: "short",
+	})
 
 	return firstLetterUpperCase([weekday])
 }
 
 export const getHour = (data: string) => {
 	const date = new Date(data)
-	const hour = date.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })
-
-	return hour
+	return date.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })
 }
