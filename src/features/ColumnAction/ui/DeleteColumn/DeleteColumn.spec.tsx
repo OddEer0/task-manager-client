477b345e50b5fc53694bd589bee0a447bcc0e19f
OddEer0 <OddEer0@gmail.com>
2023-08-31 13:19:02 +0300

    [refactor-862kb3bvg]: refactor confirm

--- src/features/ColumnAction/ui/DeleteColumn/DeleteColumn.spec.tsx
+++ src/features/ColumnAction/ui/DeleteColumn/DeleteColumn.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { fork } from "effector"
 import { Provider } from "effector-react"
@@ -7,12 +7,13 @@ import { DeleteColumn } from "@/features/ColumnAction"
 
 import { $columns, mockColumns } from "@/shared/api"
 import { MODAL_CONFIRM_OK } from "@/shared/lib"
+import { renderWithConfirm } from "@/shared/test"
 
 describe("DeleteColumn component testing", () => {
 	const testId = "test-id"
 
 	it("Should render component", () => {
-		render(<DeleteColumn id={mockColumns[0].id} data-testid={testId} />)
+		renderWithConfirm(<DeleteColumn id={mockColumns[0].id} data-testid={testId} />)
 		expect(screen.getByTestId(testId)).toBeInTheDocument()
 	})
 
@@ -20,7 +21,7 @@ describe("DeleteColumn component testing", () => {
 		const scope = fork({
 			values: [[$columns, mockColumns]],
 		})
-		render(
+		renderWithConfirm(
 			<Provider value={scope}>
 				<DeleteColumn id={scope.getState($columns)[0].id} data-testid={testId} />
 			</Provider>,
@@ -33,7 +34,9 @@ describe("DeleteColumn component testing", () => {
 
 	it("Should onDelete event working", async () => {
 		const fn = jest.fn()
-		render(<DeleteColumn id={mockColumns[0].id} data-testid={testId} onDelete={fn} />)
+		renderWithConfirm(
+			<DeleteColumn id={mockColumns[0].id} data-testid={testId} onDelete={fn} />,
+		)
 		await userEvent.click(screen.getByTestId(testId))
 		await userEvent.click(screen.getByText(MODAL_CONFIRM_OK))
 		expect(fn).toHaveBeenCalled()
